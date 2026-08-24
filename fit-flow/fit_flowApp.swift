655bab4f10737768b6b5c115e823c158f853754a
Gastón Gómez Mateo <gaston.gomez@averlance.com>
2026-08-24 21:19:40 +0200

    Initial Commit

---
+++ fit-flow/fit_flowApp.swift
@@ -0,0 +1,17 @@
+//
+//  fit_flowApp.swift
+//  fit-flow
+//
+//  Created by Gastón Gómez Mateo on 24/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct fit_flowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
